c77ce4cd9ddccf0ed151536ea18e8017f6fc1913
Callie Giles <anchoredinthecrown@gmail.com>
2023-07-13 12:39:50 -0400

    working with nodeList

--- index.js
+++ index.js
@@ -20,3 +20,24 @@ console.log(findHelloClass)
 
 const buttons = document.querySelectorAll("button");
 console.log(buttons);
+
+// Get a list of all `<h3>` elements
+const heading3List = document.querySelectorAll("h3");
+
+// Iterate over the list and print each one
+for (let element of heading3List.values()) {
+  console.log(element);
+}
+// OR 
+for (let i = 0; i < heading3List.length; i++) {
+    const element = heading3List[i];
+    console.log(element);
+  }
+// supporting older browsers:
+// Get a list of descriptions
+const list = document.querySelectorAll(".description-display");
+
+// Log them to the console
+Array.prototype.forEach.call(list, function (element) {
+  console.log(element);
+});
