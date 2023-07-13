275ef6a2f6a4ea5b6ff7a0c10bf9381bc62414a7
Callie Giles <anchoredinthecrown@gmail.com>
2023-07-13 12:36:59 -0400

    querySelectors and querySelectorAll

---
+++ index.js
@@ -0,0 +1,22 @@
+console.log(document);
+
+const heading = document.querySelector("h1");
+console.log(heading);
+
+const findValueClass = document.querySelector(".value")
+console.log(findValueClass)
+
+const findButtonElement = document.querySelector("button")
+console.log(findButtonElement)
+
+const findAreaClass = document.querySelector(".area")
+console.log(findAreaClass)
+
+const findStatDescendant = document.querySelector(".stat .value")
+console.log(findStatDescendant)
+
+const findHelloClass = document.querySelector(".hello")
+console.log(findHelloClass)
+
+const buttons = document.querySelectorAll("button");
+console.log(buttons);
